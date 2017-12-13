c31a0d8ac6867007da078100261948aa89c034e1
sishida <sishida@gsx.co.jp>
2017-12-13 17:00:11 +0900

    a trial added

--- SECCON2017/Vigenere3d/Vigenere3d.py
+++ SECCON2017/Vigenere3d/Vigenere3d.py
@@ -10,13 +10,11 @@ def _l(idx, s):
 def main(p, k1, k2):
     s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz_{}" # len(s) = 65
     t = [[_l((i + j) % len(s), s) for j in range(len(s))] for i in range(len(s))]
-    #  t = [[_l((i + j) % 65, s) for j in range(65)] for i in range(65)]
     i1 = 0
     i2 = 0
     c = ""
     for a in p:
         c += t[s.find(a)][s.find(k1[i1])][s.find(k2[i2])]
-        # c += t[s.find(a)][s.find(k1[i1])][s.find(k2[i2])]
         i1 = (i1 + 1) % len(k1)
         i2 = (i2 + 1) % len(k2)
     return c
